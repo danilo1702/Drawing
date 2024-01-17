f736bfe5afbb636d2ce8f0d02c6b2a00f99c5aee
Danilo Hernandez <jhernandez@tribalworldwide.gt>
2024-01-17 12:34:00 -0500

    update init

--- Sources/Drawing/Model/LineModel.swift
+++ Sources/Drawing/Model/LineModel.swift
@@ -12,4 +12,9 @@ public struct LineModel {
     var points = [CGPoint]()
     var color: Color = .black
     var lineWidth: Double = 1.0
+    public init(points: [CGPoint] = [CGPoint](), color: Color = .black, lineWidth: Double = 1.0) {
+        self.points = points
+        self.color = color
+        self.lineWidth = lineWidth
+    }
 }
